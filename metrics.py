e7d146594e255915ac7dc7f35c5a9daf4e44fbb9
Bulbaberry <wyc8094@gmail.com>
2022-03-12 10:30:51 -0800

    Add Error Metrics
    Co-authored-by: Yu-Chen Wang <yucwang@yucwang.ics.uci.edu>
    
    Add metrics described in the paper.
    - RMSE
    - AARE

---
+++ metrics.py
@@ -0,0 +1,12 @@
+# Copyright @yucwang 2022
+
+import numpy as np
+
+def log_rmse(y, y_reference):
+    log_diff = np.log(np.divide(y, y_reference))
+    log_mse = np.square(log_diff).mean()
+    return np.sqrt(log_mse)
+
+def aare(y, y_reference):
+    relative_diff = np.abs(np.divide(y, y_reference) - 1.)
+    return relative_diff.mean()
