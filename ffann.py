fa34c4c50ff5fc9d29f52eaca55c3ecdf8468036
Yu-Chen Wang <yuchew23@uci.edu>
2022-03-13 11:23:28 -0700

    update_weights

--- ffann.py
+++ ffann.py
@@ -10,12 +10,14 @@ import os
 class FFANN(nn.Module):
     def __init__(self, input_size, hidden_layer_size, weights=None):
         super(FFANN, self).__init__()
-        self.hidden_layer = nn.Linear(input_size, hidden_layer_size, bias=False)
-        self.out_layer = nn.Linear(hidden_layer_size, 1, bias = False)
+        self.hidden_layer = nn.Linear(input_size, hidden_layer_size, bias=True)
+        self.out_layer = nn.Linear(hidden_layer_size, 1, bias = True)
         if weights is not None:
             with torch.no_grad():
                 self.hidden_layer.weight.copy_(torch.tensor(weights['hidden_layer']))
+                self.hidden_layer.bias.copy_(torch.tensor(weights['hidden_layer_bias']))
                 self.out_layer.weight.copy_(torch.tensor(weights['output_layer']))
+                self.out_layer.bias.copy_(torch.tensor(weights['output_layer_bias']))
 
     def forward(self, x):
         hidden = self.hidden_layer(x)
@@ -67,6 +69,8 @@ def test(input_x, hidden_layer_size, weights, ref_y=None):
 weights = np.genfromtxt("./weights/ffann4_weights.txt", delimiter=None)
 weights = {
         "hidden_layer": weights[0:72,:].T,
-        "output_layer": weights[-1,:].T
+        "hidden_layer_bias": weights[72,:].T,
+        "output_layer": weights[-2,:].T,
+        "output_layer_bias": weights[-1,0]
         }
 network = FFANN(72, 4, weights=weights)
